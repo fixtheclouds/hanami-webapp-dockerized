d50b1f8d9a173e77bc513a3f1fc4baa9378b48ed
Egor Romanov <unsaidxpl@gmail.com>
2021-01-20 00:29:36 +0300

    FIx config & migration

--- db/migrations/20201219183520_create_posts.rb
+++ db/migrations/20201219183520_create_posts.rb
@@ -1,9 +1,11 @@
 Hanami::Model.migration do
   change do
-    primary_key :id
+    create_table :posts do
+      primary_key :id
 
-    column :title, String, null: false
-    column :body, String, null: false
-    column :created_at, Time, null: false
+      column :title, String, null: false
+      column :body, String, null: false
+      column :created_at, Time, null: false
+    end
   end
 end
