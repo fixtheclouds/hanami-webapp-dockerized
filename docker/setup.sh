b68480d13aa5d82d4dd5e1758e8fe9bbcd0a07b0
Egor Romanov <unsaidxpl@gmail.com>
2021-02-03 10:28:01 +0300

    Update scripts

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+# Create .env and fill missing values
+ADMIN_PASSWORD = $(cat /dev/urandom | tr -dc a-zA-Z0-9 | fold -w 14 | head -n 1)
+cp .env.example .env
+echo "ADMIN_PASSWORD=${ADMIN_PASSWORD}" >> .env
+
 docker-compose exec web bundle exec hanami db prepare
 docker-compose exec web bundle exec rake populate:all
